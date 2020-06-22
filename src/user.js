926bbeb2e8debb82b1b5b2b6bfbf857b40b59b5d
June <june39971@gmail.com>
2020-06-23 00:46:53 +0200

    fix

--- src/user.js
+++ src/user.js
@@ -3,7 +3,7 @@ module.exports = class User{
         constructor(id, name) {
         this._id = null;
         this.userid = id;
-        this.name = name;
+        this.username = name;
         this.rolls = [];
         this.lastRoll = null;
     }
