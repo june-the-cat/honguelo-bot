2ae0eb8aeac82c362c1f6fda14ea3c8cc4c8537f
june-the-cat <67217321+june-the-cat@users.noreply.github.com>
2020-07-23 23:20:26 +0200

    Revert "Revert "1.2.0""

--- src/user.js
+++ src/user.js
@@ -5,6 +5,7 @@ module.exports = class User {
         this.average = 0;
         this.userid = id;
         this.best_roll = 0;
+        this.worse_roll = 5001;
         this.username = name;
         this.lastRoll = null;
     }
